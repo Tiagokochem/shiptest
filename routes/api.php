6c747d433f15cbb097f7d2f42e84804002d408b4
tiagokochem <tiagok989@gmail.com>
2025-05-31 15:50:16 -0300

    Add API routing support and define API routes for contacts
    - Updated app.php to include the new api.php routing file.
    - Created api.php to define API routes for the ContactController, enhancing the application's API structure.
    - Added comments to api.php for better documentation of the API routes.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
-<?php   
+<?php
 
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+|
+*/
 
 Route::apiResource('contacts', ContactController::class);
