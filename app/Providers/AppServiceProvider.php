df123f4d38b7d4c01ab135ec5285945cd99fc797
tiagokochem <tiagok989@gmail.com>
2025-05-31 11:48:09 -0300

    Bind ContactRepositoryInterface to EloquentContactRepository and ContactServiceInterface to ContactService in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,39 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\ContactRepositoryInterface;
+use App\Repositories\EloquentContactRepository;
+use App\Services\ContactServiceInterface;
+use App\Services\ContactService;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        //
+        // Repositório
+        $this->app->bind(
+            ContactRepositoryInterface::class,
+            EloquentContactRepository::class
+        );
+
+        // Service
+        $this->app->bind(
+            ContactServiceInterface::class,
+            ContactService::class
+        );
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         //
     }
